c4002bc3fd06871aaae3280d4251b6b2f8be2289
Zarah <devvick230385@gmail.com>
2024-01-15 15:20:28 +0100

    added lexer for variables

--- Contract.sol
+++ Contract.sol
@@ -1,2 +1,10 @@
+// SPDX-License-Identifier: Unlicense
+// pragma solidity ^0.8.23;
 
-    uint256 myNum = 1 + 6;
+// contract Name {
+    uint8 public myNumber = 255;
+    string  myString = "Hello world"
+    bool public isWorking = false;
+
+    // function myFunction() public {}
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,136 @@
 use regex::Regex;
-use std::{env, fs};
+use std::{env, fs, process};
 
-// Define the token types
 #[derive(Debug)]
 enum Token {
-    UInt256,
-    Identifier(String),
-    Assign,
-    IntegerLiteral(i32),
-    SemiColon,
-
-    OpenParen,
-    CloseParen,
-    StringLiteral,
-    OpenArm,
-    CloseArm,
-    // Add more token types as needed
+    VariableIdentifier(String, String, String, String),
+    //DATATYPE, VISIBILITY, NAME, VALUE;
 }
 
-fn lex(input: &str) -> Vec<Token> {
-    // Define regular expressions for tokens
-    let uint256_regex = Regex::new(r"uint256").unwrap();
-    let identifier_regex = Regex::new(r"[a-zA-Z_]\w*").unwrap();
-    let assign_regex = Regex::new(r"=").unwrap();
-    let int_literal_regex = Regex::new(r"\d+").unwrap();
-    let semi_colon_literal_regex = Regex::new(r";").unwrap();
-
-    let mut tokens = Vec::new();
-    let mut remaining_input = input;
-
-    // println!("{remaining_input}");
-
-    while !remaining_input.is_empty() {
-        // Skip whitespaces
-        // if let Some(whitespace_match) = Regex::new(r"\s+").unwrap().find(remaining_input) {
-        //     let whitespace_end = whitespace_match.end();
-        //     remaining_input = &remaining_input[whitespace_end..];
-        //     println!("space here {:?} {remaining_input}", whitespace_match);
-        //     continue;
-        // }
-
-        // Match uint256
-        if let Some(uint256_match) = uint256_regex.find(remaining_input) {
-            let uint256_end = uint256_match.end();
-            tokens.push(Token::UInt256);
-            remaining_input = &remaining_input[uint256_end..];
-            continue;
-        }
-
-        // Match identifier
-        if let Some(identifier_match) = identifier_regex.find(remaining_input) {
-            let identifier_end = identifier_match.end();
-            let identifier_str = &remaining_input[..identifier_end].trim();
-            tokens.push(Token::Identifier(identifier_str.to_string()));
-            remaining_input = &remaining_input[identifier_end..];
-            continue;
-        }
-
-        //Match assignment operator
-        if let Some(assign_match) = assign_regex.find(remaining_input) {
-            let assign_end = assign_match.end();
-            tokens.push(Token::Assign);
-            remaining_input = &remaining_input[assign_end..];
-            continue;
-        }
-
-        //Match SemiColon
-        if let Some(assign_match) = semi_colon_literal_regex.find(remaining_input) {
-            let assign_end = assign_match.end();
-            tokens.push(Token::SemiColon);
-            remaining_input = &remaining_input[assign_end..];
-            continue;
-        }
-
-        //Match integer literal
-        if let Some(int_match) = int_literal_regex.find(remaining_input) {
-            let int_end = int_match.end();
-            let int_str = &remaining_input[..int_end].trim();
-            let int_value = int_str.parse::<i32>().unwrap();
-            tokens.push(Token::IntegerLiteral(int_value));
-            remaining_input = &remaining_input[int_end..];
-            continue;
-        }
-
-        // Handle unrecognized characters
-        panic!(
-            "Unexpected character: {}",
-            remaining_input.chars().next().unwrap()
-        );
-    }
-
-    tokens
-}
-
+const DATA_TYPES: [&str; 10] = [
+    "uint8", "uint16", "uint32", "uint256", "int8", "int16", "int32", "int256", "bool", "string",
+];
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("ERROR: Compiler needs a file path");
+        process::exit(1);
+    }
 
-    let scripts: &[String] = &args[1..];
-    // let file = fs::read_to_string(&scripts[0]).expect("Could not read file");
-    let input = "uint256 myNum = 5;";
+    if args[1].split(".").last().unwrap() != "sol" {
+        eprintln!("ERROR: Invalid file format");
+        process::exit(1);
+    }
 
-    let tokens = lex(input.trim());
-    println!("{tokens:?}");
+    let file_content = fs::read_to_string(&args[1]).unwrap_or_else(|err| {
+        eprintln!("ERROR: Error opening file <{}>. {err}", args[1]);
+        process::exit(1);
+    });
 
-    // let lines = file.lines();
+    let mut program_lines: Vec<&str> = vec![];
 
-    // let oi: Vec<_> = lines.filter(|f| !f.starts_with("//")).collect();
-    // let mut opend_braces = 0;
-    // let mut chars = String::new();
-    // for line in oi.iter().enumerate() {
-    //     for jj in line.1.chars() {
-    //         if jj == '{' {
-    //             opend_braces += 1;
-    //         }
-    //         if jj == '}' {
-    //             opend_braces -= 1;
-    //         }
-    //         chars.push(jj);
-    //     }
-    // }
-    // println!(" {chars:?} {opend_braces}")
+    for line in file_content
+        .lines()
+        .filter(|ft| !ft.trim().starts_with("//") && !ft.is_empty())
+    {
+        let check_double: Vec<&str> = line.trim().split(";").collect();
+
+        for db_lines in check_double {
+            // println!("{db_lines}")
+            if !db_lines.trim().is_empty() {
+                program_lines.push(db_lines.trim());
+            }
+        }
+
+        // check_double.map(|e| if !e.trim().is_empty() {})
+    }
+
+    // println!("{program_lines:?}");
+    let mut tokens = Vec::new();
+
+    for pr_line in &program_lines {
+        // println!("{pr_line}")
+        // lex(pr_line);
+        tokens.push(variable_lexing(&pr_line));
+        // let token: Token = variable_lexing(&pr_line);
+    }
+    println!("{:?}", tokens);
+
+    // println!("{program_lines:?} {}", program_lines[3]);
+}
+
+fn variable_lexing(input: &str) -> Token {
+    let identifier_regex = Regex::new(r"[a-zA-Z_]\w*").unwrap();
+    let int_literal_regex = Regex::new(r"\d+").unwrap();
+    let bool_literal_regex = Regex::new(r"\b(true|false)\b").unwrap();
+    let string_literal_regex = Regex::new(r#""([^"]*)""#).unwrap();
+    let line_arm: Vec<&str> = input.split(" ").collect();
+
+    let mut data_type: Option<String> = None;
+    let mut visibility: Option<String> = None;
+    let mut name: Option<String> = None;
+    let mut value: Option<String> = None;
+
+    if !DATA_TYPES.contains(&line_arm[0]) {
+        eprintln!("ERROR: Unidentified data type {}", line_arm[0]);
+        process::exit(1);
+    } else {
+        let blah = line_arm[1..].join(" ");
+        // println!("{} {}", line_arm.join(" "), line_arm.len());
+        data_type = Some(line_arm[0].to_string());
+        if let Some(identifier_match) = &identifier_regex.find(&blah) {
+            if identifier_match.as_str() == "public" {
+                if let Some(blah_match) = identifier_regex.find(&blah[identifier_match.end()..]) {
+                    name = Some(blah_match.as_str().to_string())
+                }
+            } else {
+                name = Some(identifier_match.as_str().to_string())
+            }
+        }
+
+        if let Some(visibility_match) = Regex::new(r"public").unwrap().find(&blah) {
+            visibility = Some(visibility_match.as_str().to_string());
+        }
+
+        if let Some(int_match) = int_literal_regex.find(&blah) {
+            value = Some(int_match.as_str().to_string());
+        }
+
+        if let Some(string_match) = string_literal_regex.find(&blah) {
+            value = Some(string_match.as_str().to_string());
+        }
+
+        if let Some(bool_match) = bool_literal_regex.find(&blah) {
+            value = Some(bool_match.as_str().to_string());
+        }
+
+        if let None = data_type {
+            print_error("Invalid syntax");
+        }
+
+        if let None = visibility {
+            visibility = Some("private".to_string());
+        }
+
+        if let None = name {
+            print_error("Variable name required");
+        }
+
+        if let None = value {
+            print_error("Missing value for variable")
+        }
+
+        let token = Token::VariableIdentifier(
+            data_type.unwrap(),
+            visibility.unwrap(),
+            name.unwrap(),
+            value.unwrap(),
+        );
+        token
+    }
+}
+
+fn print_error(msg: &str) {
+    eprintln!("ERROR: {msg}");
+    process::exit(1);
 }
